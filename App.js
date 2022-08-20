593989e28ea2704139c5244ebae136b121a31461
Maaz Junior <goalmaaz@gmail.com>
2022-08-20 22:11:50 +0100

    testing my react-native

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.TextColor}>My First React Native Application</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#323",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  TextColor: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
