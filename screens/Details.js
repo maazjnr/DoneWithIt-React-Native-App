98ce229d195ef459869e67abef91b3f7d495d35f
Maaz Junior <goalmaaz@gmail.com>
2022-08-26 01:57:16 +0100

    go back navigation

--- screens/Details.js
+++ screens/Details.js
@@ -1,10 +1,36 @@
+import React from "react";
 import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from "react-native"
-
 import {COLORS, SIZES, SHADOWS, FONTS, assets} from '../constants'
 import {CircleButton, RectButton, SubInfo, 
 FocusStatusBar, DetailsDesc, DetailsBid} from '../components'
 
+const DetailsHeader = ({ data, navigation }) => (
+    <View style={{ width: "100%", height: 373 }}>
+      <Image
+        source={data.image}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%" }}
+      />
+  
+      <CircleButton
+        imgUrl={assets.left}
+        handlePress={() => navigation.goBack()}
+        left={15}
+        top={StatusBar.currentHeight + 10}
+      />
+  
+      <CircleButton
+        imgUrl={assets.heart}
+        right={15}
+        top={StatusBar.currentHeight + 10}
+      />
+    </View>
+  );
+  
+
 const Details = ({route, navigation}) => {
+
+    const { data } = route.params;
 
     return(
         <SafeAreaView style={{flex: 1}}>
@@ -33,6 +59,11 @@ const Details = ({route, navigation}) => {
             keyExtractor={(item) => item.id}
             showVerticalScrollIndicator={false}
             contentContainerStyle={{paddingBottom: SIZES.extraLarge * 3}}
+            ListHeaderComponent={() => (
+                <React.Fragment>
+                    <DetailsHeader data={data} navigation={navigation} />
+                </React.Fragment>
+            )}
             />
         </SafeAreaView>
     )
