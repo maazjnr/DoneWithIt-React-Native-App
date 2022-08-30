183a334e9cfc75f2ab006f7373f29ebc2a55ebfa
Maaz Junior <goalmaaz@gmail.com>
2022-08-30 15:24:22 +0100

    final commit

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,6 +1,7 @@
 import { View, Text, Image, TextInput } from "react-native";
 import { COLORS, FONTS, SIZES, assets } from "../constants";
 
+
 const HomeHeader = ({ onSearch }) => {
   return (
     <View
